1cd5bbdd2e03977ffd617f3ce91d4632de82d047
Sabyata <sabyatakarki05@gmail.com>
2026-07-29 15:30:10 +0545

    user respo code

--- server/src/repository/user.repository.ts
+++ server/src/repository/user.repository.ts
@@ -8,7 +8,6 @@ export interface IUserRepository {
   createUser(userData: Partial<IUser>): Promise<IUser>;
   getUserById(id: string): Promise<IUser | null>;
 
-  //this is the get all users
   getAllUsers(
     page: number,
     size: number,
